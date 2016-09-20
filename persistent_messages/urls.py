591549e0f6dc03a392511a07d10e811117da86c6
Aldiantoro Nugroho <kriwil@gmail.com>
2016-09-20 14:24:45 +0700

    update urls to use Django 1.8 rule

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('',
-    url(r'^detail/(?P<message_id>\d+)/$', 'persistent_messages.views.message_detail', name='message_detail'),
-    url(r'^mark_read/(?P<message_id>\d+)/$', 'persistent_messages.views.message_mark_read', name='message_mark_read'),
-    url(r'^mark_read/all/$', 'persistent_messages.views.message_mark_all_read', name='message_mark_all_read'),
-)
+from persistent_messages.views import message_detail, message_mark_read, message_mark_all_read
+
+urlpatterns = [
+    url(r'^detail/(?P<message_id>\d+)/$', message_detail, name='message_detail'),
+    url(r'^mark_read/(?P<message_id>\d+)/$', message_mark_read, name='message_mark_read'),
+    url(r'^mark_read/all/$', message_mark_all_read, name='message_mark_all_read'),
+]
